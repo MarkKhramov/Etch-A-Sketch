49e0ad4f05c1c1829ccab1b5698ccbde0a475f37
Mark K <markkhramov@outlook.com>
2020-04-20 12:34:34 -0600

    Made functions so that code would be easier to read

--- EASscript.js
+++ EASscript.js
@@ -1,30 +1,35 @@
-totalPx = 960;
+const TOTALPX = 960;
+var container = document.getElementById("container");
 
-function createBlocks(num)
+
+function createBoard(num)
 {
-    var container = document.getElementById("container");
-    var sideLength = totalPx/num;
-    container.setAttribute("style","display:grid;  grid-template-columns: repeat(" + num +"," + sideLength + "px); grid-template-rows: repeat(" + num + "," +sideLength +"px);");
     
-    var block;
+    var sideLength = TOTALPX/num;
 
-    for(let i = 0; i < (num*num); i++)
-    {
-        block = null;
-        block = document.createElement("div");
-        block.setAttribute("class", "blck");
-        block.setAttribute("style", "height:" + sideLength + "px;" + " " + "width:" + sideLength + "px;");
-        container.appendChild(block);
-        
-    }
+    container.setAttribute("style", makeBoardAttributes(num,sideLength));
+    
+    createBlocks(num,sideLength);
    
     const blockArray = Array.from(container.getElementsByClassName("blck"));
-    console.log(blockArray.length);
-    
     blockArray.forEach(blck => blck.addEventListener('mouseover', () => blck.setAttribute("class", "blck blocks")));
 
 }
 
+function makeBoardAttributes(num, sideLength)
+{
+    return "display:grid; grid-template-columns: repeat(" + num +"," + sideLength + "px); grid-template-rows: repeat(" + num + "," + sideLength +"px);";
+}
 
-
-createBlocks(100)
+function createBlocks(numPerSide, sideLength)
+{
+    var block;
+    for(let i = 0; i < (numPerSide*numPerSide); i++)
+    {
+        block = document.createElement("div");
+        block.setAttribute("class", "blck");
+        block.setAttribute("style", "height:" + sideLength + "px;" + " " + "width:" + sideLength + "px;");
+        container.appendChild(block);  
+    }
+}
+createBoard(100)
