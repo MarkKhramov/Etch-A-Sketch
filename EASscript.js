e36be608096261593b5329bfea1f052930c612df
Mark K <markkhramov@outlook.com>
2020-04-20 11:34:34 -0600

    Added blocks

--- EASscript.js
+++ EASscript.js
@@ -1,14 +1,30 @@
+totalPx = 960;
+
 function createBlocks(num)
 {
-    var container = document.getElementById("container")
-    container.style.gridTemplateColumns = num;
-    container.style.gridTemplateRows = num;
+    var container = document.getElementById("container");
+    var sideLength = totalPx/num;
+    container.setAttribute("style","display:grid;  grid-template-columns: repeat(" + num +"," + sideLength + "px); grid-template-rows: repeat(" + num + "," +sideLength +"px);");
     
-    for(let i = 0; i < num; i++)
-    {
-        container.appendChild = document.createElement('div').setAttribute("class", "blocks");
-    }
+    var block;
 
+    for(let i = 0; i < (num*num); i++)
+    {
+        block = null;
+        block = document.createElement("div");
+        block.setAttribute("class", "blck");
+        block.setAttribute("style", "height:" + sideLength + "px;" + " " + "width:" + sideLength + "px;");
+        container.appendChild(block);
+        
+    }
+   
+    const blockArray = Array.from(container.getElementsByClassName("blck"));
+    console.log(blockArray.length);
     
+    blockArray.forEach(blck => blck.addEventListener('mouseover', () => blck.setAttribute("class", "blck blocks")));
+
 }
-createBlocks(16)
+
+
+
+createBlocks(100)
